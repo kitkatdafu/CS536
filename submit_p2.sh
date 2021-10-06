9438f3f98379a38b9126f8507ab86ca176bbff6a
Reid Chen <mrr1vfe@gmail.com>
2021-10-06 01:10:09 -0500

    update

---
+++ submit_p2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+zip -r chen.reid.hu.gaoyi.P2.zip ./p2/deps ./p2/b.jlex ./p2/ErrMsg.java ./p2/Makefile ./p2/P2.java ./p2/sym.java ./p2/chen.reid.hu.gaoyi.P2.pdf ./p2/inputs ./p2/outputs ./p2/expects
+
