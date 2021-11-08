3590f1633032270d4efb5e3af4e8f0617342c01d
Reid Chen <mrr1vfe@gmail.com>
2021-11-08 10:55:29 -0600

    add subclasses to track structures of functions and structs

--- p4/Symb.java
+++ p4/Symb.java
@@ -1,15 +1,70 @@
+import java.util.*;
+
 public class Symb {
     private String type;
+    private String kind;
 
-    public Symb(String type) {
+    public static final String STRUCT = "struct";
+    public static final String FUNC = "func";
+    public static final String VAR = "var";
+
+    public Symb(String type, String kind) {
         this.type = type;
+        this.kind = kind;
     }
-    
+
+    public boolean isStruct() {
+        return this.kind.equals(STRUCT);
+    }
+
+    public String getKind() {
+        return this.kind;
+    }
+
     public String getType() {
         return type;
     }
-    
+
     public String toString() {
         return type;
+    }
+}
+
+class FnSymb extends Symb {
+
+    private FormalsListNode formals;
+    private String fnName;
+
+    public FnSymb(String type, String fnName, FormalsListNode formals) {
+        super(type, Symb.FUNC);
+        this.fnName = fnName;
+        this.formals = formals;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder bld = new StringBuilder();
+
+        bld.append(this.fnName);
+        bld.append("(");
+        bld.append(this.formals.toString());
+        bld.append("->");
+        bld.append(this.getType());
+        bld.append(")");
+
+        return bld.toString();
+
+    }
+}
+
+class StructSymb extends Symb {
+
+    private String structName;
+    private DeclListNode structFields;
+
+    public StructSymb(String type, String structName, DeclListNode structFields) {
+        super(type, Symb.STRUCT)；
+        this.structName = structName;
+        this.structFields = structFields;
     }
 }
