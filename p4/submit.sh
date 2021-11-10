bfd181c3a54aa9a723fe1c0c6dcaae898222fcd8
Reid Chen <mrr1vfe@gmail.com>
2021-11-10 00:25:40 -0600

    update

--- p4/submit.sh
+++ p4/submit.sh
@@ -7,18 +7,18 @@ FIRSTNAME2="GAOYI"
 
 
 zip -r $LASTNAME1.$FIRSTNAME1.$LASTNAME2$.FIRSTNAME2.P4.zip\
-deps/\
-ast.java\
-b.cup\
-b.jlex\
-DuplicateSymException.java\
-EmptySymTableException.java\
-WrongArgumentException.java\
-ErrMsg.java\
-Makefile\
-P4.java\
-Sym.java\
-SymTable.java\
-nameErrors.b\
-test.b\
-$LASTNAME1.$FIRSTNAME1.$LASTNAME2.$FIRSTNAME2.P4.pdf
+ deps/\
+ ast.java\
+ b.cup\
+ b.jlex\
+ DuplicateSymException.java\
+ EmptySymTableException.java\
+ WrongArgumentException.java\
+ ErrMsg.java\
+ Makefile\
+ P4.java\
+ Sym.java\
+ SymTable.java\
+ nameErrors.b\
+ test.b\
+ $LASTNAME1.$FIRSTNAME1.$LASTNAME2.$FIRSTNAME2.P4.pdf
