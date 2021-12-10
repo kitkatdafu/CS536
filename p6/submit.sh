29fa5e2c3bba303707cadebe9d0487057dbae8fa
Yi Chen <mrr1vfe@gmail.com>
2021-12-10 15:47:10 -0600

    update submission

--- p6/submit.sh
+++ p6/submit.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+lastname1=chen
+firstname1=reid
+lastname2=hu
+firstname2=gaoyi
+
+zip -r $lastname1.$firstname1.$lastname2.$firstname2.P6.zip \
+  deps/ \
+  ast.java \
+  b.cup \
+  b.jlex \
+  Codegen.java \
+  DuplicateSymException.java \
+  EmptySymTableException.java \
+  WrongArgumentException.java \
+  ErrMsg.java \
+  Makefile \
+  P6.java \
+  Symb.java \
+  SymTable.java \
+  Type.java \
+  $lastname1.$firstname1.$lastname2.$firstname2.P6.pdf \
